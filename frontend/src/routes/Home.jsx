c59877705608a71e34e18c0e92208172efc0a283
MVKoleva21 <MVKoleva21@codingburgas.bg>
2024-01-13 07:29:33 +0200

    Implement landing page

---
+++ frontend/src/routes/Home.jsx
@@ -0,0 +1,157 @@
+import React from "react";
+import 'tailwindcss/tailwind.css';
+
+export default function Home() {
+  return (
+    <div className="bg-[#0a0b0e] flex flex-row justify-center w-full">
+      <div className="bg-blackblack-5 overflow-hidden w-[1555px] h-[4086px] relative">
+        <div className="absolute w-[2604px] h-[4242px] top-[-367px] left-[-660px]">
+          <div className="absolute w-[2604px] h-[4242px] top-0 left-0">
+            <div className="absolute w-[629px] h-[1096px] top-[3146px] left-[1730px]">
+              <div className="sticky w-[545px] h-[1096px] top-0 left-[84px] rounded-[272.5px/548px] rotate-180 blur-[350px] [background:linear-gradient(180deg,rgba(112.2,0,255,0)_0%,rgb(255,41.44,195.2)_100%)]">
+                <div className="sticky w-[279px] h-[480px] top-[69px] left-[173px] blur-[200px] [background:linear-gradient(180deg,rgba(24,75,255,0)_0%,rgb(23.37,74.33,255)_100%)]" />
+              </div>
+              <img
+                className="absolute w-[133px] h-[193px] top-[185px] left-0"
+                alt="Star"
+                src="/public/star-1.svg"
+              />
+            </div>
+            <div className="absolute w-[1599px] h-[1482px] top-[2171px] left-[529px]">
+              <div className="sticky w-[564px] h-[708px] top-[141px] left-0 rounded-[282px/354px] rotate-180 blur-[200px] [background:linear-gradient(180deg,rgba(255,0,0,0)_0%,rgb(255,118.32,41.44)_100%)]" />
+              <img
+                className="absolute w-[198px] h-[259px] top-[82px] left-[311px]"
+                alt="Star"
+                src="/public/star-2.svg"
+              />
+              <img
+                className="absolute w-[54px] h-[46px] top-[302px] left-[228px]"
+                alt="Star"
+                src="/public/star-3.svg"
+              />
+              <div className="flex flex-col w-[896px] items-start justify-center gap-[32px] absolute top-[557px] left-[703px]">
+                <p className="relative self-stretch mt-[-1.00px] font-h2-50px font-[number:var(--h2-50px-font-weight)] text-blackblack-100 text-[length:var(--h2-50px-font-size)] text-right tracking-[var(--h2-50px-letter-spacing)] leading-[var(--h2-50px-line-height)] [font-style:var(--h2-50px-font-style)]">
+                  Connect with your loved ones and start exploring
+                </p>
+                <p className="relative self-stretch [font-family:'Inter',Helvetica] font-normal text-blackblack-60 text-[20px] text-right tracking-[0] leading-[30px]">
+                  Connect with your loved ones and start bequeathing <br />
+                  your digital assets to them.
+                  <br />
+                  <br />
+                  Our user-friendly interface makes the transition to a digital will effortless, providing you with a
+                  reliable and accessible solution for your estate planning needs.
+                </p>
+              </div>
+              <img
+                className="absolute w-[620px] h-[1482px] top-0 left-[131px]"
+                alt="Looper"
+                src="/public/looper-1.png"
+              />
+            </div>
+            <div className="absolute w-[2604px] h-[2583px] top-0 left-0">
+              <img
+                className="absolute w-[715px] h-[1045px] top-[1538px] left-[1500px]"
+                alt="Looper"
+                src="/public/looper.png"
+              />
+              <div className="flex w-[780px] items-center top-[1511px] left-[1115px] flex-col gap-[32px] absolute">
+                <div className="inline-flex flex-col items-center gap-[16px] relative flex-[0_0_auto]">
+                  <p className="relative w-[780px] mt-[-1.00px] [font-family:'Inter',Helvetica] font-black text-blackblack-100 text-[55px] text-center tracking-[0] leading-[71.5px]">
+                    A digital will platform that ensures your legacy
+                  </p>
+                  <p className="relative w-[515px] [font-family:'Poppins',Helvetica] font-normal text-blackblack-60 text-[20px] text-center tracking-[0] leading-[normal]">
+                    <span className="font-medium">
+                      Welcome to the new modern will - digital will.
+                      <br />
+                      <br />
+                      Unlock a new world with our digital will site and give a try to the latest discovery in the field
+                      of wills.
+                      <br />
+                      <br />
+                    </span>
+                    <span className="[font-family:'Poppins',Helvetica] font-normal text-[#898ca8] text-[20px] tracking-[0]">
+                      Step into the future of testamentary preparation with confidence, convenience, and peace of mind.
+                      <br />
+                    </span>
+                  </p>
+                </div>
+              </div>
+              <img
+                className="absolute w-[61px] h-[60px] top-[1769px] left-[1970px]"
+                alt="Star"
+                src="/public/star-4.svg"
+              />
+              <div className="absolute w-[2560px] h-[1584px] top-0 left-0">
+                <div className="relative w-[1555px] h-[1005px] top-[367px] left-[660px]">
+                  <img
+                    className="absolute w-[1158px] h-[578px] top-[136px] left-[41px] blur-[200px]"
+                    alt="Circle three"
+                    src="/public/circle-three.svg"
+                  />
+                  <img
+                    className="absolute w-[1461px] h-[840px] top-[5px] left-0"
+                    alt="Circle two"
+                    src="/public/circle-two.svg"
+                  />
+                  <img
+                    className="absolute w-[1555px] h-[1005px] top-0 left-0"
+                    alt="Circle one"
+                    src="/public/circle-one.svg"
+                  />
+                </div>
+              </div>
+              <div className="absolute w-[354px] h-[382px] top-[286px] left-[705px] rotate-180">
+                <div className="relative h-[382px]">
+                  <div className="sticky w-[354px] h-[334px] top-[48px] left-0 rounded-[177.16px/166.85px] blur-[200px] [background:linear-gradient(180deg,rgb(23.26,58.74,127.62)_0%,rgb(16.7,87.42,61.96)_100%)]" />
+                  <div className="sticky w-[170px] h-[227px] top-0 left-[92px] rounded-[282px/354px] blur-[200px] [background:linear-gradient(180deg,rgba(24,255,157.98,0)_0%,rgb(23.38,255,227.2)_100%)]" />
+                </div>
+              </div>
+              <img
+                className="absolute w-[61px] h-[60px] top-[1088px] left-[1915px]"
+                alt="Star img"
+                src="/public/star-4.svg"
+              />
+              <img
+                className="absolute w-[61px] h-[60px] top-[608px] left-[849px]"
+                alt="Star"
+                src="/public/star-4.svg"
+              />
+              <div className="sticky w-[603px] h-[603px] top-[518px] left-[2001px] rounded-[301.5px] blur-[200px] [background:linear-gradient(180deg,rgba(0,193.8,255,0)_0%,rgb(255,41.44,195.2)_100%)]" />
+              <div className="inline-flex items-center top-[763px] left-[1048px] flex-col gap-[32px] absolute">
+                <div className="inline-flex flex-col items-center gap-[16px] relative flex-[0_0_auto]">
+                  <div className="relative w-[780px] mt-[-1.00px] font-h1-85px font-[number:var(--h1-85px-font-weight)] text-blackblack-100 text-[length:var(--h1-85px-font-size)] text-center tracking-[var(--h1-85px-letter-spacing)] leading-[var(--h1-85px-line-height)] [font-style:var(--h1-85px-font-style)]">
+                    VALIUM
+                  </div>
+                  <div className="relative w-[483px] font-body-16px font-[number:var(--body-16px-font-weight)] text-blackblack-60 text-[length:var(--body-16px-font-size)] text-center tracking-[var(--body-16px-letter-spacing)] leading-[var(--body-16px-line-height)] [font-style:var(--body-16px-font-style)]">
+                    digital will platform
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex w-[896px] items-start justify-center top-[3822px] left-[797px] flex-col gap-[32px] absolute">
+            <p className="relative self-stretch mt-[-1.00px] font-h2-50px font-[number:var(--h2-50px-font-weight)] text-blackblack-100 text-[length:var(--h2-50px-font-size)] tracking-[var(--h2-50px-letter-spacing)] leading-[var(--h2-50px-line-height)] [font-style:var(--h2-50px-font-style)]">
+              Take your first step <br />
+              into digital will
+            </p>
+            <p className="relative self-stretch [font-family:'Inter',Helvetica] font-normal text-blackblack-60 text-[20px] tracking-[0] leading-[30px]">
+              Make your first step into the digital realm, our platform seamlessly guides you through the process of
+              creating a comprehensive and secure digital will. Experience the convenience of managing your estate
+              online, ensuring your wishes are safeguarded.
+            </p>
+          </div>
+        </div>
+        <img
+          className="absolute w-[198px] h-[259px] top-[4053px] left-[2791px]"
+          alt="Star"
+          src="/public/star-5.svg"
+        />
+        <img
+          className="absolute w-[61px] h-[60px] top-[3875px] left-[1009px]"
+          alt="Star"
+          src="/public/star-4.svg"
+        />
+      </div>
+    </div>
+  );
+};
