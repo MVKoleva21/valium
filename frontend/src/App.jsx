4d61cb0d23b1c61cc1c0d1eee1379dbbeb206d35
IYStoychev21 <IYStoychev21@codingburgas.bg>
2024-01-14 17:35:08 +0200

    Add sign up page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,11 +4,13 @@ import { createBrowserRouter, RouterProvider} from 'react-router-dom'
 import Home from './routes/Home'
 import Protected from './components/Protected'
 import SignIn from './routes/SignIn'
+import SignUp from './routes/SignUp'
 
 export default function App() {
   const BrowserRouter = createBrowserRouter([
     { path: '/', element: <Home /> },
     { path: '/signin', element: <SignIn /> },
+    { path: '/signup', element: <SignUp /> },
   ])
 
   return (
